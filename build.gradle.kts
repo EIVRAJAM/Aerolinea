9946db49bd951dfe6bd8d3c229bccbbacabdda5c
ABRAHAM <abrahamceballosnegocios@gmail.com>
2024-11-22 20:32:25 -0500

    Cambios de dependencias y modificaciones generales

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,6 +4,10 @@ plugins {
     id("io.spring.dependency-management") version "1.1.6"
 }
 
+springBoot {
+    mainClass = "model.AerolineaModelsApplication"
+}
+
 group = "com.example"
 version = "0.0.1-SNAPSHOT"
 
@@ -28,16 +32,19 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-security")
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-web-services")
+    implementation("org.mapstruct:mapstruct:1.5.3.Final")
     compileOnly("org.projectlombok:lombok")
-    "developmentOnly"("org.springframework.boot:spring-boot-devtools")
     runtimeOnly("org.postgresql:postgresql")
     annotationProcessor("org.projectlombok:lombok")
+    annotationProcessor("org.mapstruct:mapstruct-processor:1.5.3.Final")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.springframework.security:spring-security-test")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
-    implementation("org.mapstruct:mapstruct:1.5.5.Final")
-    annotationProcessor("org.mapstruct:mapstruct-processor:1.5.5.Final")
+    implementation ("io.jsonwebtoken:jjwt-api:0.12.6")
+    runtimeOnly ("io.jsonwebtoken:jjwt-impl:0.12.6")
+    runtimeOnly ("io.jsonwebtoken:jjwt-jackson:0.12.6")
 }
+
 
 tasks.withType<Test> {
     useJUnitPlatform()
--- src/main/java/model/AerolineaModelsApplication.java
+++ src/main/java/model/AerolineaModelsApplication.java
@@ -1,11 +1,16 @@
+package model;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AerolineaModelsApplication {
+public class
+AerolineaModelsApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(AerolineaModelsApplication.class, args);
     }
 
 }
+
+
